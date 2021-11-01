5b43e39aebc979d609df3e77705718d271d1543f
KhoiNM <minhnguyenkhoi@gmail.com>
2021-11-01 11:56:07 +0700

    #khoinm #update seo configs

--- constants/SeoConfigs.js
+++ constants/SeoConfigs.js
@@ -1,7 +1,7 @@
 const webUrl = 'https://omicrm.vn/', previewUrl = `${webUrl}static/preview/`;
 
 const defaultConfig = {
-    title: `Giải pháp CRM toàn diện`,
+    title: `Nền tảng quản lý giao tiếp đa kênh`,
     description: 'Tổng đài thông minh sử dụng trên điện toán đám mây(Cloudphone) nền tảng internet(VoIP) - nâng cao chất lượng cuộc gọi, quản lý khách hàng dễ dàng bằng CRM có sẵn',
     image: `${previewUrl}intro.png`,
     canonical: webUrl,
